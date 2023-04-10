480862632e31b2e7fd4198d841b8e59adbd066ff
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-10 15:02:43 +0700

    fix autoupdate

--- app/entity/default.go
+++ app/entity/default.go
@@ -4,5 +4,5 @@ import "time"
 
 type GORMModel struct {
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at,omitempty"`
-	UpdatedAt time.Time `gorm:"autoCreateTime" json:"updated_at,omitempty"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at,omitempty"`
 }
